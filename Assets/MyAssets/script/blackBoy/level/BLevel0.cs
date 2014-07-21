5840fd44693c026d40f0f41bd3e9850870fb7311
AtwoodDeng <new_146675@163.com>
2014-07-20 17:34:41 -0700

    update level0

--- Assets/MyAssets/script/blackBoy/level/BLevel0.cs
+++ Assets/MyAssets/script/blackBoy/level/BLevel0.cs
@@ -42,80 +42,80 @@ public class BLevel0 : BLevel {
 	
 	public void OnMouseClick(EventDefine eventName, object sender, EventArgs args)
 	{
-		MessageEventArgs msg = (MessageEventArgs)args;
-		if (  Global.MouseLeft.Equals( msg.GetMessage("type") ) )
-		{
-			if ( ! isClick )
-			{
-				isClick = true;
-				showTipsByKey( "CLICK" );
-			}
-		}
+//		MessageEventArgs msg = (MessageEventArgs)args;
+//		if (  Global.MouseLeft.Equals( msg.GetMessage("type") ) )
+//		{
+//			if ( ! isClick )
+//			{
+//				isClick = true;
+//				showTipsByKey( "CLICK" );
+//			}
+//		}
 
 	}
 	public void OnCatch(EventDefine eventName, object sender, EventArgs args)
 	{
-		Catchable catchable;
-		if ( ( catchable = BObjManager.Instance.BHeroBody.leftHandGroup.getCatchable() ) == null )
-		{
-			if  ( ( catchable = BObjManager.Instance.BHeroBody.rightHandGroup.getCatchable() ) == null )
-			{
-				return;
-			}
-		}
-
-		BlackWhiteRock rock = (BlackWhiteRock)catchable;
-		if ( rock != null )
-		{
-
-			if ( rock.spinType == BlackWhiteRock.SpinType.White )
-			{
-				if ( ! isWhite )
-				{
-					isWhite = true;
-					showTipsByKey( "WHITE" );
-				}
-			} else if ( rock.spinType == BlackWhiteRock.SpinType.Black )
-			{
-				if ( ! isBlack )
-				{
-					isBlack = true;
-					showTipsByKey( "BLACK" );
-				}
-			}
-		}
+//		Catchable catchable;
+//		if ( ( catchable = BObjManager.Instance.BHeroBody.leftHandGroup.getCatchable() ) == null )
+//		{
+//			if  ( ( catchable = BObjManager.Instance.BHeroBody.rightHandGroup.getCatchable() ) == null )
+//			{
+//				return;
+//			}
+//		}
+//
+//		BlackWhiteRock rock = (BlackWhiteRock)catchable;
+//		if ( rock != null )
+//		{
+//
+//			if ( rock.spinType == BlackWhiteRock.SpinType.White )
+//			{
+//				if ( ! isWhite )
+//				{
+//					isWhite = true;
+//					showTipsByKey( "WHITE" );
+//				}
+//			} else if ( rock.spinType == BlackWhiteRock.SpinType.Black )
+//			{
+//				if ( ! isBlack )
+//				{
+//					isBlack = true;
+//					showTipsByKey( "BLACK" );
+//				}
+//			}
+//		}
 
 	}
 	public override void DealWith (float deltaTime)
 	{
-		switch( state )
-		{
-		case State.Init:
-			showNextDialogGroup();
-			state =  State.Play;
-			break;
-		case State.Play:
-			if ( halo_count >= maxHalo )
-			{
-				state = State.End;
-			}
-			break;
-		case State.End:
-			Application.LoadLevel("begin");
-			break;
-		}
+//		switch( state )
+//		{
+//		case State.Init:
+//			showNextDialogGroup();
+//			state =  State.Play;
+//			break;
+//		case State.Play:
+//			if ( halo_count >= maxHalo )
+//			{
+//				state = State.End;
+//			}
+//			break;
+//		case State.End:
+//			Application.LoadLevel("begin");
+//			break;
+//		}
 	}
 
 	public override void DealTrigger (string msg)
 	{
 		if ( "out_of_range".Equals( msg ) )
 		{
-			if ( ! isOutOfRange )
-			{
-				isOutOfRange = true;
-				
-				showTipsByKey( "OUT" );
-			}
+//			if ( ! isOutOfRange )
+//			{
+//				isOutOfRange = true;
+//				
+//				showTipsByKey( "OUT" );
+//			}
 			HeroBody body = BObjManager.Instance.BHeroBody;
 			body.transform.position = startPoints[1].transform.position;
 			Vector3 velocity = body.rigidbody.velocity;
@@ -123,20 +123,20 @@ public class BLevel0 : BLevel {
 			velocity.y = 0;
 			body.rigidbody.velocity = velocity;
 		}
-		if ( "get_mind".Equals( msg ) )
-		{	
-			halo_count++;
-
-			if ( !isMind )
-			{
-				isMind = true;
-				showTipsByKey( "HALO" );
-			}else {
-				
-				string tips = BDataManager.Instance.getDialogWithKey( levelName , "MIND" );
-				tips += "(" + halo_count.ToString() + "/" + maxHalo.ToString() + ")";
-				showTips( tips , Global.TipsShowTime , Global.TipsDisappearTime );
-			}
-		}
+//		if ( "get_mind".Equals( msg ) )
+//		{	
+//			halo_count++;
+//
+//			if ( !isMind )
+//			{
+//				isMind = true;
+//				showTipsByKey( "HALO" );
+//			}else {
+//				
+//				string tips = BDataManager.Instance.getDialogWithKey( levelName , "MIND" );
+//				tips += "(" + halo_count.ToString() + "/" + maxHalo.ToString() + ")";
+//				showTips( tips , Global.TipsShowTime , Global.TipsDisappearTime );
+//			}
+//		}
 	}
 }
